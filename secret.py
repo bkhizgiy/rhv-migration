827e131ab2dbe9f067a02adb2679855b9f7b4bb3
Bella Khizgiyaev <bkhizgiy@redhat.com>
2023-03-23 15:05:32 +0200

    Adding scripts for secret and provider creatio
    Signed-off-by: Bella Khizgiyaev <bkhizgiy@redhat.com>

--- secret.py
+++ secret.py
@@ -8,21 +8,26 @@ config.load_kube_config()
 user = "<user>"
 password = "<pass>"
 url = "<url>/ovir-engine/api"
-insecure = "True"
+insecure = "false"
+with open('ca.pem', 'r') as f:
+    cacert = f.read()
+
 user = encoded_value = base64.b64encode(user.encode("utf-8")).decode("utf-8")
 password = encoded_value = base64.b64encode(password.encode("utf-8")).decode("utf-8")
 url = encoded_value = base64.b64encode(url.encode("utf-8")).decode("utf-8")
 insecure = encoded_value = base64.b64encode(insecure.encode("utf-8")).decode("utf-8")
+cacert = encoded_value = base64.b64encode(cacert.encode("utf-8")).decode("utf-8")
 
 # Create the Secret object
 secret = client.V1Secret(
-    metadata=client.V1ObjectMeta(name="my-secret"),
+    metadata=client.V1ObjectMeta(name="my-secret1", labels={"createdForProviderType": "ovirt"}),
     type="Opaque",
     data={
         "username": user,
         "password": password,
         "url": url,
-        "insecureSkipVerify": insecure
+        "insecureSkipVerify": insecure,
+        "cacert": cacert
     }
 )
 
