aa9c281670bd3ff23387aff788ea045390b7e730
Bella Khizgiyaev <bkhizgiy@redhat.com>
2023-03-28 20:01:18 +0300

    Add OS vairable to set all the componenmt names in the system.
    Signed-off-by: Bella Khizgiyaev <bkhizgiy@redhat.com>

--- create_provider.py
+++ create_provider.py
@@ -1,4 +1,5 @@
 from kubernetes import client, config
+import os
 
 config.load_kube_config()
 
@@ -6,21 +7,22 @@ config.load_kube_config()
 api_cr_client = client.ApiClient()
 custom_api = client.CustomObjectsApi(api_cr_client)
 
-secret_name = "secret-name"
+secret_name = os.environ.get('SECRET')
+mtv_namespace = os.environ.get('NAMESPACE')
+provider_name = os.environ.get('PROVIDER')
 
 #create provider object
-provider_name = "provider-script"
 cr_object = {
     "apiVersion": "forklift.konveyor.io/v1beta1",
     "kind": "Provider",
     "metadata": {
         "name": provider_name,
-        "namespace": "konveyor-forklift",
+        "namespace": mtv_namespace,
     },
     "spec": {
         "secret": {
             "name": secret_name,
-            "namespace": "konveyor-forklift",
+            "namespace": mtv_namespace,
         },
         "type": "ovirt",
         "url": "https://vm-10-122.lab.eng.tlv2.redhat.com/ovirt-engine/api",
@@ -31,7 +33,7 @@ cr_object = {
 custom_api.create_namespaced_custom_object(
     group="forklift.konveyor.io",
     version="v1beta1",
-    namespace="konveyor-forklift",
+    namespace=mtv_namespace,
     plural="providers",
     body=cr_object
 )
--- run_full_setup.sh
+++ run_full_setup.sh
@@ -4,6 +4,16 @@ GREEN='\033[0;32m'
 PUR='\033[0;35m'
 NC='\033[0m' # No Color
 
+export NAMESPACE=${NAMESPACE:-}
+export SECRET=${SECRET:-mtv-secret}
+export PROVIDER=${PROVIDER:-mtv-provider}
+export STORAGE_MAP=${STORAGE_MAP:-mtv-storage-mapping}
+export NETWORK_MAP=${NETWORK_MAP:-mtv-network-mapping}
+
+provider_name = os.environ.get('PROVIDER')
+storage_mapping_name = os.environ.get('STORAGE_MAP')
+network_mapping_name = os.environ.get('NETWORK_MAP')
+
 echo -e "${GREEN}-------Starting setting up the env for migration-------${NC}"
 python create_secret.py
 echo -e "${GREEN}1.-------Secret created-------${NC}"
